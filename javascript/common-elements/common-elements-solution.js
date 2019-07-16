cb9433d717deb425dee6dac5cb016f64a94c724c
Michael Agard <mdagard@gmail.com>
2019-07-16 19:35:10 -0400

    add helpful comments for common-elements-solution

--- javascript/common-elements/common-elements-solution.js
+++ javascript/common-elements/common-elements-solution.js
@@ -5,11 +5,17 @@ let array2B = ['x', 'y', 'z', 'a'];
 let array3A = [1, 2, 3];
 let array3B = [4, 5, 6];
 
+// Function that will return the common elements between two arrays
 function commonElements(arrayA, arrayB) {
-  let outPut = [];
+	// Instantiate variable outPut as an empty array
+	let outPut = [];
+	// For loop to iterate through arrayA
 	for (let i = 0; i < arrayA.length; i++) {
+		// For loop to iterate through arrayB
 	  for (let j = 0; j < arrayB.length; j++) {
+			// Conditional to compare if arrayA[i] and arrayB[j] are exact matches and if the outPut array doesn't already contain a duplicate of that match 
 	    if (arrayA[i] === arrayB[j] && !outPut.includes(arrayA[i])) {
+				// Append arrayA[i] to the outPut array
 	      outPut.push(arrayA[i]);
 	    }
 	  }
